c552f348ba2b8a8b3199ac9050cea52a5655f18f
diaoshe <heyuhuade@gmail.com>
2016-07-27 12:17:03 +0800

    es test3 search

--- elastictest3.py
+++ elastictest3.py
@@ -4,6 +4,11 @@ from datetime import datetime
 if __name__ == '__main__':
     print 'r'*30
     es = Elasticsearch([{'host': '10.174.93.111', 'port': '9200'}])
-    es_one = es.get(index='ksxing', doc_type='testqm', id='5545c306f3805c7730c35fad')['_source']
-    for i in es_one:
-        print i, es_one.get(i)
+    #  es_one = es.get(index='ksxing', doc_type='testqm', id='5545c306f3805c7730c35fad')['_source']
+    #  for i in es_one:
+    #      print i, es_one.get(i)
+
+    es_search_one = es.search(index='ksxing', doc_type='testqm', body={
+        'query': {"match_all": {}},
+        "size": 1
+    })
