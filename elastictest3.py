ea0a2800d497829bc91b919f53de2661cc2c26ad
diaoshe <heyuhuade@gmail.com>
2016-07-27 11:20:00 +0800

    es test3

--- elastictest3.py
+++ elastictest3.py
@@ -3,6 +3,6 @@ from datetime import datetime
 
 if __name__ == '__main__':
 
-    es = Elasticsearch({'host':'10.174.93.111','port'9200})
-    es_one = es.get(index='ksxing',doc_type='testqm',id='5545c306f3805c7730c35fad')
+    es = Elasticsearch({'host': '10.174.93.111', 'port': '9200'})
+    es_one = es.get(index='ksxing', doc_type='testqm', id='5545c306f3805c7730c35fad')
     print "es_one:", es_one
