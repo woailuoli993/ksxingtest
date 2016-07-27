f9719c8a373df1e67d535130e08d031593670e8c
diaoshe <heyuhuade@gmail.com>
2016-07-27 11:37:02 +0800

    es test3

--- elastictest3.py
+++ elastictest3.py
@@ -5,4 +5,5 @@ if __name__ == '__main__':
     print 'r'*30
     es = Elasticsearch([{'host': '10.174.93.111', 'port': '9200'}])
     es_one = es.get(index='ksxing', doc_type='testqm', id='5545c306f3805c7730c35fad')['_source']
-    print "es_one:", es_one.decode('utf-8')
+    for i in es_one:
+        print i, es_one.get(i)
