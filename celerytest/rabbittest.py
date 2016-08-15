48b6526e8258e31fb7a8b9e71b7618b508cd4fbe
diaoshe <heyuhuade@gmail.com>
2016-08-15 17:48:39 +0800

    single commit diaoshe

---
+++ celerytest/rabbittest.py
@@ -0,0 +1,21 @@
+# _*_ coding:utf-8 _*_
+
+import pika
+from celery import Celery
+
+
+# celery 相关配置  队列地址  结果存放地址 消息格式
+CELERY_BROKER_URL = 'amqp://ksxing@ksxing@10.174.93.111:5672'
+CELERY_RESULT_BACKEND = 'mongodb://10.174.93.111:27017/celery'
+CELERY_TASK_SERIALIZER = 'json'
+
+app = Celery(__name__, broker=CELERY_BROKER_URL, backend=CELERY_RESULT_BACKEND)
+
+
+@app.task()
+def add(x, y):
+    return x + y
+
+
+if __name__=='__main__':
+    pass
