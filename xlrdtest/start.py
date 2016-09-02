ebd5ee034f29f315707ca5267bc7fe35868ea1c3
diaoshe <heyuhuade@gmail.com>
2016-09-02 17:08:28 +0800

    xlrd test

--- xlrdtest/start.py
+++ xlrdtest/start.py
@@ -3,5 +3,17 @@ import xlrd
 """
 xlrd 急速入门
 """
-def test():
 
+
+def test():
+    # 打开excel
+    data = xlrd.open_workbook('test.xls')
+
+    # 查看sheet names
+    print data.sheet_names()
+
+    # 获取工作表的几种方法
+    table = data.sheets()[0]
+    table2 = data.sheet_by_index(0)
+
+    print table==table2
