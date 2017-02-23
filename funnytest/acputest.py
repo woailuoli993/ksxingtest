ee5118682b0cfc526c08795f4b400ae7f54b6490
vici <heyuhuade@gmail.com>
2017-02-23 11:18:37 +0800

    idea change

--- funnytest/acputest.py
+++ funnytest/acputest.py
@@ -1,9 +1,16 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 # Created by vici on 15/02/2017
+from dosth.testthread import timeit
 
-
-
-
+@timeit
+def cputest():
+    su = 0
+    for i in range(1, 40001):
+        for j in range(1, 40001):
+            su += i * j
+    print(su)
 if __name__ == '__main__':
-    pass
+
+    cputest()
+
