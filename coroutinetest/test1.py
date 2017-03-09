badd17e59be570b3300f53183e67ba9f880e5043
vici <heyuhuade@gmail.com>
2017-03-09 16:22:46 +0800

    add for github

---
+++ coroutinetest/test1.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# Created by vici on 09/03/2017
+
+
+def consumer():
+    r = ''
+    while True:
+        n = yield r
+        if not n:
+            return
+        print('[CONSUMER] Consuming %s...' % n)
+        r = '200 OK'
+
+
+def produce(c):
+    c.send(None)
+    n = 0
+    while n < 5:
+        n += 1
+        print('[PRODUCER] Producing %s...' % n)
+        r = c.send(n)
+        print('[PRODUCER] Consumer return: %s' % r)
+
+    c.close()
+
+
+if __name__ == '__main__':
+
+    c = consumer()
+    produce(c)
+    pass
---
+++ coroutinetest/testcoroutines.py
@@ -0,0 +1,48 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# Created by vici on 09/03/2017
+#  python tail -f | understand coroutines
+
+import time
+
+
+# coroutine decorator
+def coroutine(func):
+    def start(*args, **kwargs):
+        cr = func(*args, **kwargs)
+        cr.next()
+        return cr
+    return start
+
+
+def follow(thefile, target):
+    print "start tail"
+    thefile.seek(0, 2)
+    # seek(x, y ) the default is 0, means file beginning, 1 => current position, 2=> end of file
+    while True:
+        line = thefile.readline()
+        if not line:
+            time.sleep(0.1)
+            continue
+        print("[+] get 1 new line")
+        target.send(line)
+
+
+@coroutine
+def printer():
+    while True:
+        line = (yield)
+        print line,
+
+
+if __name__ == '__main__':
+    import platform
+    print platform.python_version()
+    f = open("1", 'rb')
+    print f.readline()
+    f.seek(0)
+    printt = printer()
+    follow(f, printt)
+
+
+
