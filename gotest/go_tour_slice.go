87c6c80a6d84a38caa3f5cf7dea396804d11bb6b
vici <heyuhuade@gmail.com>
2017-03-13 15:39:54 +0800

    add for github

--- gotest/go_tour_slice.go
+++ gotest/go_tour_slice.go
@@ -1,32 +1,25 @@
 package main
 
-import "fmt"
-//import "golang.org/x/tour/pic"
+import (
+	"golang.org/x/tour/pic"
+	"math"
+)
 
 //实现 Pic 。它应当返回一个长度为 dy 的切片，其中每个元素是一个长度为 dx ，元素类型为 uint8 的切片
 
 func Pic(dx, dy int) [][]uint8 {
 	myret := make([][]uint8, dy)
 	for i := 0; i < dy ; i++{
+		myret[i] = make([]uint8, dx)
 		for j := 0; j < dx ; j++ {
-			myret := append(myret[i], uint8(dy*i+j))
+			myret[i][j] = uint8(float64(j) * math.Log(float64(i)))
 		}
 
 	}
 	return myret
 }
 
-//func Pic(dx, dy int) [][]uint8 {
-//	return [][]uint8{
-//		[]uint8{uint8(dx), 2, 3},
-//		[]uint8{uint8(dy), 2, 3},
-//		[]uint8{3, 2, 3},
-//		[]uint8{4, 2, 3},
-//
-//	}
-//}
 
 func main() {
-	fmt.Println(Pic(10, 10))
-	//pic.Show(Pic)
+	pic.Show(Pic)
 }
