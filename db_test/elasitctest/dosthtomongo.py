9f6ce7c2e1430ee954d296f7fe9e487792299d56
vici <heyuhuade@gmail.com>
2017-11-15 18:32:34 +0800

    mongo>?

--- db_test/elasitctest/dosthtomongo.py
+++ db_test/elasitctest/dosthtomongo.py
@@ -1,12 +1,28 @@
-# _*_ coding:utf-8 _*_
-
-from pymongo import MongoClient
-from bson.objectid import ObjectId
-
+# -*- coding: utf-8 -*-
+from datetime import datetime
+from pymongo import MongoClient, bulk
 
 client = MongoClient('localhost', 27017)
+db = client['local']
+collection = db.test_collection
 
-db = client.train
-collection = db.protestcol
-find = collection.find_one({'_id':ObjectId('5790330064325c1f57185e62')})
-print find
+
+class OrderTable(object):
+
+    @staticmethod
+    def insert(shop_id: int,
+               package_id: int,
+               user_location: 'user gps data',
+               created_at: datetime) -> bulk.ObjectId:
+
+        """
+        :return: created mongo order id
+        """
+        return collection.posts.insert_one(locals()).inserted_id
+
+
+if __name__ == '__main__':
+    now = datetime.now()
+    id = OrderTable.insert(123, 1, [111, 222], now)
+    print(id)
+    print(type(id))
