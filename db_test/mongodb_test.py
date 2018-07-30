64ffd158a5e8a57d765ac78da8d25bb9b3fb7656
vici <heyuhuade@gmail.com>
2018-07-30 22:09:04 +0800

    auto

--- db_test/mongodb_test.py
+++ db_test/mongodb_test.py
@@ -5,7 +5,7 @@ from mongoengine import (
     Document,
     StringField,
     IntField,
-
+    DecimalField
 )
 from mongoengine.context_managers import (
     switch_collection
@@ -19,18 +19,20 @@ connection_2 = connect(db='test', alias='test')
 
 class Student(Document):
 
-    name = StringField()
+    name = StringField(min_length=5)
     age = IntField()
 
     meta = {'db_alias': 'test'}
 
 
 if __name__ == '__main__':
-    # with switch_collection(Student, 'test') as Student:
-    #     Student(name='yuhua', age='10101010').save()
-    client = pymongo.MongoClient()
-    print(client.list_database_names())
-    db = client['moen']
-    print(dir)
+    with switch_collection(Student, 'test') as Student:
+        stu = Student.objects(name='not age').first()
+        print(stu.name)
+        print(stu.age)
+    # client = pymongo.MongoClient()
+    # print(client.list_database_names())
+    # db = client['moen']
+    # print(dir)
 
 
